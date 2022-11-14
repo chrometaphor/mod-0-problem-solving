8f920f8844d0f0f05b82db7f83c4b8d7ecd08f71
Chris Cole <christopher.a.cole@gmail.com>
2022-11-14 12:16:28 -0800

    add notes to practice2

---
+++ practice2.js
@@ -0,0 +1,14 @@
+// 1. Start with an array of strings with a mix of 
+// uppercase and lowercase letters. Print every word 
+// in all lowercase letters.
+
+var words = ["CaTs", "PeOpLe", "DoGs"]
+
+for (var i = 0; i < words.length; i++) {
+    console.log(words[i].toLowerCase())
+}
+
+// use variable of words to indicate arrays
+// use a for statement  to set arrays
+// use a method "length" to add my "ing"
+// console the words with log
