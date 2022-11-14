19a25c07c3c943e83edeeced976ae693289e252c
Chris Cole <christopher.a.cole@gmail.com>
2022-11-14 11:03:29 -0800

    add changes to practice 1

---
+++ practice1.js
@@ -0,0 +1,17 @@
+// <!-- Solve at least 3 of the challenges below. Create a file 
+// using the language for your program 
+// (`challenge1.rb` or `challenge1.js`) for each of the 
+// challenges you select. For each challenge you complete, 
+// include comments showing the **overall goal** in your own words, 
+// your **pseudocode**, and your **final solution**.  -->
+
+// <!-- 1. Given an array of strings, return only the strings that have exactly 4 characters. -->
+// function four() {
+
+
+    var four = ["laugh", "eat", "play", "cats"]
+    for (i == 0; i = four.length; i++) {
+        if (four[i].charLength === 4) {
+            console.log(four[i])
+        }
+    }
