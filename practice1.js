1cf941438faa32b217846df7e2d1a6da21131ad7
Chris Cole <christopher.a.cole@gmail.com>
2022-11-14 12:21:03 -0800

    cleanup notes

--- practice1.js
+++ practice1.js
@@ -22,8 +22,3 @@
 
 
 
-    // 1. Start with an array of strings with a mix of uppercase and lowercase letters. 
-    // Print every word in all lowercase letters.
-
-    // var mix = ["mix", "SHIRT", "day", "LIFT"]
-
